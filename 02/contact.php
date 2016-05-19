7cee1f263cd32ab77580916fec035682093a2be8
t-sato <takass.1220@gmail.com>
2016-05-19 15:57:23 +0900

    input　screen

--- 02/contact.php
+++ 02/contact.php
@@ -1,10 +1,15 @@
+<Center>
 <form action="result.php" method="post">
 
 性:<input type = "textbox" name="name1" size="20">
 <br>
 名:<input type = "textbox" name="name2" size="20">
 <br>
-住所:<input type = "textbox" name="address" size="18">
+性別：<input  type = "radio" name="rdo" value =0 checked >不明
+<input type = "radio" name="rdo" value =1 >男
+<input type = "radio" name="rdo" value =2 >女
+<br>
+住所:<input type = "textbox" name="address" size="20">
 <br>
 電話番号：<input type = "textbox" name="tell1" size="20">
 -<input type = "textbox" name="tell2" size="20">
@@ -14,9 +19,22 @@
 E-mail:<input type = "textbox" name="mail1" size="20">
 @<input type = "textbox" name="mail2" size="20">
 <br>
-性別：<input  type = "radio" name="rdo" value =0 checked >不明
-<input type = "radio" name="rdo" value =1 >男
-<input type = "radio" name="rdo" value =2 >女
-<br>
+こちらのサイトをどこで知りましたか？ <br>
+<input type="hidden" name="where" value="0">
+<input type="checkbox" name="where[]" value="インターネット" >インターネット
+<input type="checkbox" name="where[]" value="CM">CM
+<input type="checkbox" name="where[]" value="チラシ">チラシ
+<input type="checkbox" name="where[]" value="知人の紹介">知人の紹介
+    <br>
+    <select name="question">
+        <option value="お仕事の依頼について">お仕事の依頼について</option>
+<option value="製品の購入について">製品の購入について</option>
+<option value="製品について">製品について</option>
+<option value="採用について">採用について</option>
+        </select>
+        <br>
+        <textarea name="inquiry" rows="10" cols="40" placeholder="ここにお問い合せ内容を記入してください。"></textarea>
+        <br>
 <input type ="submit">
 </form>
+</Center>
--- 02/result.php
+++ 02/result.php
@@ -1,10 +1,37 @@
 <?php
 echo "名前：".$_POST['name1']." ".$_POST['name2']."<br>";
+
+ $sex=$_POST['rdo'];
+ echo "性別：　";
+if($sex == 0 ){
+    echo "不明<br>";
+}elseif($sex == 1){
+        echo "男<br>";
+    }elseif($sex == 2){
+        echo "女<br>";
+    }
+
+
 echo "住所：".$_POST['address']."<br>";
 
 echo "電話番号：".$_POST['tell1']."-".$_POST['tell2']."-".$_POST['tell3']."<br>";
 echo "E-mail:".$_POST['mail1']."@".$_POST['mail2']."<br>";
 
-echo $_POST['rdo'];
+echo "知った理由";
+
+$aa = $_POST['where'] ;
+if(0<$aa){
+$n = count($aa);
+for($i=0;$i<$n; $i++){
+echo "：".$aa[$i]." ";
+}
+}else{
+    echo "：なし";
+}
+echo "<br>";
+
+echo "質問内容：".$_POST['question']."<br>";
+
+echo $_POST['inquiry'];
 
  ?>
