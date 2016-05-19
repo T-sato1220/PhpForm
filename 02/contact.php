aab97c05a4ca8c9bd107fba3db87fb26fa5a5133
t-sato <takass.1220@gmail.com>
2016-05-19 14:12:32 +0900

    afternoon sex,where

---
+++ 02/contact.php
@@ -0,0 +1,22 @@
+<form action="result.php" method="post">
+
+性:<input type = "textbox" name="name1" size="20">
+<br>
+名:<input type = "textbox" name="name2" size="20">
+<br>
+住所:<input type = "textbox" name="address" size="18">
+<br>
+電話番号：<input type = "textbox" name="tell1" size="20">
+-<input type = "textbox" name="tell2" size="20">
+-<input type = "textbox" name="tell3" size="20">
+
+<br>
+E-mail:<input type = "textbox" name="mail1" size="20">
+@<input type = "textbox" name="mail2" size="20">
+<br>
+性別：<input  type = "radio" name="rdo" value =0 checked >不明
+<input type = "radio" name="rdo" value =1 >男
+<input type = "radio" name="rdo" value =2 >女
+<br>
+<input type ="submit">
+</form>
---
+++ 02/result.php
@@ -0,0 +1,10 @@
+<?php
+echo "名前：".$_POST['name1']." ".$_POST['name2']."<br>";
+echo "住所：".$_POST['address']."<br>";
+
+echo "電話番号：".$_POST['tell1']."-".$_POST['tell2']."-".$_POST['tell3']."<br>";
+echo "E-mail:".$_POST['mail1']."@".$_POST['mail2']."<br>";
+
+echo $_POST['rdo'];
+
+ ?>
